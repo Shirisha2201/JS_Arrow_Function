68ac56873cc00fb4ed9e6b85a064ef62c28b89df
Shirisha <shirishabaddal2201@gmail.com>
2025-11-18 15:10:06 +0530

    Template Literals & Shorthands

---
+++ template_literals.js
@@ -0,0 +1,19 @@
+const username = "Shirisha";
+const course = "JavaScript";
+
+console.log(`Hello ${username}, welcome to the ${course} course!`)
+
+const name = "Sam";
+const age = 21;
+const student = {
+    name,
+    age,
+    greet(){
+        console.log("Hello");
+    }
+};
+student.greet();
+console.log(student);
+
+const getFullName = (first, last) => `${first} ${last}`;
+console.log(getFullName("Sam", "Wilson"));
